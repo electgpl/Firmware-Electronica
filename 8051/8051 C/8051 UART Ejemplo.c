90ad1cab7d737c245e0ebb68f345821a1ea74b18
Sebastian Caccavallo <electgpl@gmail.com>
2017-10-26 20:23:03 -0300

    Update 8051 UART Ejemplo.c

--- 8051/8051 C/8051 UART Ejemplo.c
+++ 8051/8051 C/8051 UART Ejemplo.c
@@ -1,5 +1,5 @@
 #include<reg52.h>
-#define Baud_rate 0xFD  									//BAUD RATE 9600                     
+#define BAUDRATE 0xFD                             //BAUD RATE 9600                     
 sbit P1_0 = P1^0;
 sbit P1_1 = P1^1;
 sbit P1_2 = P1^2;
@@ -8,18 +8,18 @@ sbit P1_3 = P1^3;
 // Funcion para inicializar el puerto serie 9600 @ 11.059MHz
 //*********************************************************************************************************
 void serialInit(void){                   	
-   TMOD = 0x20;                           			//Timer 1 en modo 2 - Auto recarga para generar Baudrate
-   SCON = 0x50;                           			//Serial modo 1, 8bit de dato, 1bit de start, 1bit de stop
-   TH1 = Baud_rate;                       			//Carga el baudrate en el timer
-   TR1 = 1;                               			//Dispara el timer
+   TMOD = 0x20;                                    //Timer 1 en modo 2 - Auto recarga para generar Baudrate
+   SCON = 0x50;                                    //Serial modo 1, 8bit de dato, 1bit de start, 1bit de stop
+   TH1 = BAUDRATE;                                 //Carga el baudrate en el timer
+   TR1 = 1;                                        //Dispara el timer
 }
 //*********************************************************************************************************
 // Funcion para enviar byte por puerto serie
 //*********************************************************************************************************
 void sendByte(unsigned char serialdata){
-   SBUF = serialdata;                        		//Carga el dato a enviar por uart
-   while(TI == 0);                            	   //Espera a la transmicion completa
-   TI = 0;                                    	   //Borra el flag de transmision
+   SBUF = serialdata;                              //Carga el dato a enviar por uart
+   while(TI == 0);                                 //Espera a la transmicion completa
+   TI = 0;                                         //Borra el flag de transmision
 }
 //*********************************************************************************************************
 // Interrupcion de byte presente en puerto serie
@@ -47,8 +47,8 @@ void serial_ISR (void) interrupt 4{
 // Funcion principal
 //*********************************************************************************************************
 void main(){
-   P1 = 0x00; 												//Usado para aplicacion
-   P3 = 0x03; 												//Usado para el serie
+   P1 = 0x00;                                    //Usado para aplicacion
+   P3 = 0x03;                                    //Usado para el serie
    serialInit();    
    EA = 1;
    ES = 1;
