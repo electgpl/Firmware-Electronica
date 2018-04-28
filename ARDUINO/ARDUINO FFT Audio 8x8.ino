c39ff8737ce70bc0b9d8f411c1ed36c4d37bf920
Sebastian Caccavallo <electgpl@gmail.com>
2018-04-28 13:27:17 -0300

    Update ARDUINO FFT Audio 8x8.ino

--- ARDUINO/ARDUINO FFT Audio 8x8.ino
+++ ARDUINO/ARDUINO FFT Audio 8x8.ino
@@ -2,12 +2,9 @@
 #include "arduinoFFT.h"
 
 const uint16_t samples = 16;
+uint16_t k = 0;
 double vReal[samples];
 double vImag[samples];
-#define SCL_INDEX 0x00
-#define SCL_TIME 0x01
-#define SCL_FREQUENCY 0x02
-#define SCL_PLOT 0x03
 
 arduinoFFT FFT = arduinoFFT();
 LedControl lc = LedControl(12,11,10,1);
@@ -24,33 +21,37 @@ void loop(){
       vImag[i] = 0;
    }
    
-   FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+   FFT.Windowing(vReal, samples, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
    FFT.Compute(vReal, vImag, samples, FFT_FORWARD);
    FFT.ComplexToMagnitude(vReal, vImag, samples);
-   
-   for(uint16_t j = 0; j < (samples >> 1); j++){
-      if((int)vReal[j]<10)
-         lc.setRow(0,j,1);
+
+   k = (samples/2)-1;
+
+   for(uint16_t j = 0; j < (samples/2); j++){
+      if((byte)vReal[j]<10)
+         lc.setRow(0,k,1);
       else 
-         if((int)vReal[j]<30)
-            lc.setRow(0,j,3);
+         if((byte)vReal[j]<30)
+            lc.setRow(0,k,3);
          else 
-            if((int)vReal[j]<50)
-               lc.setRow(0,j,7);
+            if((byte)vReal[j]<50)
+               lc.setRow(0,k,7);
             else 
-               if((int)vReal[j]<70)
-                  lc.setRow(0,j,15);
+               if((byte)vReal[j]<70)
+                  lc.setRow(0,k,15);
                else 
-                  if((int)vReal[j]<90)
-                     lc.setRow(0,j,31);
+                  if((byte)vReal[j]<90)
+                     lc.setRow(0,k,31);
                   else 
-                     if((int)vReal[j]<110)
-                        lc.setRow(0,j,63);
+                     if((byte)vReal[j]<110)
+                        lc.setRow(0,k,63);
                      else 
-                        if((int)vReal[j]<130)
-                           lc.setRow(0,j,127);
+                        if((byte)vReal[j]<130)
+                           lc.setRow(0,k,127);
                         else 
-                           if((int)vReal[j]<150)
-                              lc.setRow(0,j,255);    
+                           if((byte)vReal[j]<150)
+                              lc.setRow(0,k,255);   
+       if(k>0)
+          k--;
    }
 }
