e0aa1af9be0adf2fe1925dd1916a9930f1f59846
Sebastian Caccavallo <electgpl@gmail.com>
2018-09-18 20:21:43 -0300

    Create ESP_Estacion_DHT_BMP_LDR_RST.c

---
+++ ESP/ESP_Estacion_DHT_BMP_LDR_RST.c
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <SimpleDHT.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+String apiKey = "EHW19OYPTK9J7F40";
+const char* server = "api.thingspeak.com";
+int pinDHT11 = D0;
+int pinFACTORY = D6;
+int timeOut = 0;
+SimpleDHT11 dht11;
+WiFiClient client;
+Adafruit_BMP085 bmp;
+
+void handleButton(){
+   int debounce = 1000;
+   if(digitalRead(pinFACTORY)==LOW){
+      delay(debounce);
+      if(digitalRead(pinFACTORY)==LOW){
+         WiFiManager wifiManager;      
+         wifiManager.resetSettings();
+         delay(1000);
+         ESP.reset();
+      }
+   }
+}
+
+void setup(){
+   WiFiManager wifiManager;
+   Serial.begin(115200);
+   pinMode(pinFACTORY, INPUT);
+   delay(10);
+   // wifiManager.resetSettings();
+   wifiManager.autoConnect("WS-Electgpl");
+   Serial.println("Estacion Conectada a Internet");
+   if(!bmp.begin()){
+      Serial.println("Error en BMP085");
+   }
+}
+
+void loop(){
+   byte temperatura = 0;
+   byte humedad = 0;
+   float luminosidad = analogRead(A0);
+   handleButton();
+   if(dht11.read(pinDHT11, &temperatura, &humedad, NULL)){
+      Serial.print("Error en DHT11");
+      return;
+   }
+   if(timeOut==300){
+      if(client.connect(server,80)){
+         String postStr = apiKey;
+         postStr +="&field1=";
+         postStr += String(luminosidad);
+         postStr +="&field2=";
+         postStr += String((float)bmp.readTemperature());
+         postStr +="&field3=";
+         postStr += String((int)humedad);
+         postStr +="&field4=";
+         postStr += String((float)bmp.readPressure()/100);
+         postStr += "\r\n\r\n";
+         client.print("POST /update HTTP/1.1\n");
+         client.print("Host: api.thingspeak.com\n");
+         client.print("Connection: close\n");
+         client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+         client.print("Content-Type: application/x-www-form-urlencoded\n");
+         client.print("Content-Length: ");
+         client.print(postStr.length());
+         client.print("\n\n");
+         client.print(postStr);
+         Serial.println("% send to Thingspeak");
+      }
+      client.stop();
+      Serial.print("luminosidad: ");
+      Serial.print(luminosidad);
+      Serial.print(" | temperatura BMP: ");
+      Serial.print((float)bmp.readTemperature());
+      Serial.print(" | temperatura DHT: ");
+      Serial.print(temperatura);
+      Serial.print(" | humedad: ");
+      Serial.print(humedad);
+      Serial.print(" | presion: ");
+      Serial.println((float)bmp.readPressure()/100);
+      timeOut=0; 
+   }
+   delay(1000);
+   timeOut++;
+   Serial.print("timeOut");
+   Serial.println(timeOut);
+}
