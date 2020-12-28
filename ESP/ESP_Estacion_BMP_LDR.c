63de92633cd1b0d22b91463f1d4102d0c1dfc871
Electgpl <electgpl@gmail.com>
2020-12-28 16:37:11 -0300

    Create ESP_Estacion_BMP_LDR.c

---
+++ ESP/ESP_Estacion_BMP_LDR.c
@@ -0,0 +1,81 @@
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+#define SECRET_SSID "MySSID"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 0000000     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+Adafruit_BMP085 bmp;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+// Initialize our values
+int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int number4 = 0;
+String myStatus = "";
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  WiFi.mode(WIFI_STA); 
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+  if(!bmp.begin()){
+      Serial.println("Error en BMP085");
+      while(1){}
+  }
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  // set the fields with the values
+  ThingSpeak.setField(1, number1);
+  ThingSpeak.setField(2, number2);
+  ThingSpeak.setField(3, number3);
+  ThingSpeak.setField(4, number4);
+  
+  // set the status
+  ThingSpeak.setStatus(myStatus);
+  
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  // change the values
+  number1 = analogRead(A0);
+  number2 = bmp.readTemperature();
+  number3 = 0;
+  number4 = bmp.readPressure()/100;
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
